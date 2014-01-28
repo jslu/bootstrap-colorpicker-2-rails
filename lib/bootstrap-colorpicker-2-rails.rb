c84cac01ab864f316589319f95aa021f79e9bdfd
Marcelo de Moraes Serpa <fullofcaffeine@gmail.com>
2014-01-28 02:30:29 -0600

    Added all files

---
+++ lib/bootstrap-colorpicker-2-rails.rb
@@ -0,0 +1,9 @@
+require "bootstrap-colorpicker-2-rails/version"
+
+module BootstrapColorpicker2
+      module Rails 
+        class Engine < ::Rails::Engine
+        end
+      end
+end
+
---
+++ lib/bootstrap-colorpicker-2-rails/version.rb
@@ -0,0 +1,6 @@
+module BootstrapColorpicker2
+      module Rails 
+        VERSION = '2.0.0'
+      end
+end
+
